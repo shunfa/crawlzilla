39e51f9a972b1f00617cc0693a2701d462ff3b27
shunfa <shunfa@gmail.com>
2013-01-11 18:06:35 +0800

    daily commit

--- v2.1/webmanager/src/nchc/fslab/crawlzilla/bean/crawlBean.java
+++ v2.1/webmanager/src/nchc/fslab/crawlzilla/bean/crawlBean.java
@@ -1,14 +1,39 @@
 package nchc.fslab.crawlzilla.bean;
 
+import java.io.IOException;
+
 public class crawlBean {
 
-	// crawl
-	// bin/nutch crawl urls /opt/crawlzilla/crawlDB/nchc-3-5 -depth 3 -topN 5
-	
-	
-	// solrindex
-	// bin/nutch solrindex http://127.0.0.1:8983/solr/collection1 /opt/crawlzilla/crawlDB/nchc-3-5/crawldb -linkdb /opt/crawlzilla/crawlDB/nchc-3-5/linkdb /opt/crawlzilla/crawlDB/nchc-3-5/segments/*
-	
-	
-	
+	boolean crawlJob(String DBName) throws IOException {
+		String cmd = "/opt/crawlzilla/nutch/bin/nutch crawl /opt/crawlzilla/nutch/urls -dir /opt/crawlzilla/crawlDB/"
+				+ DBName + " -depth 3 -topN 5";
+		Runtime.getRuntime().exec(cmd);
+		System.out.println(cmd);
+		_makeSolrIndex(DBName);
+		return true;
+	}
+
+	// use shellscript replace this
+	boolean _makeSolrIndex(String solrName) throws IOException {
+		String cmdCpConf = "cp -r /opt/crawlzilla/solr/example/solr/collection1 /opt/crawlzilla/solr/example/solr/" + solrName;
+		// edit /opt/crawlzilla/solr/example/solr/solr.xml
+		// <core schema="schema.xml" instanceDir="nchc-from-bean/" name="nchc-from-bean" config="solrconfig.xml" dataDir="data"/>
+		Runtime.getRuntime().exec(cmdCpConf);
+		String cmd = "/opt/crawlzilla/nutch/bin/nutch solrindex http://127.0.0.1:8983/solr/"
+				+ solrName
+				+ " /opt/crawlzilla/crawlDB/"
+				+ solrName
+				+ "/crawldb -linkdb /opt/crawlzilla/crawlDB/"
+				+ solrName
+				+ "/linkdb /opt/crawlzilla/crawlDB/" + solrName + "/segments/*";
+		System.out.println(cmd);
+		return true;
+	}
+
+	public static void main(String args[]) throws IOException {
+
+		// test function ...
+		crawlBean crawlJobs = new crawlBean();
+		crawlJobs.crawlJob("nchc-from-bean");
+	}
 }
