be336b78df0976dc359bafdaa76545889e718780
shunfa <shunfa@gmail.com>
2013-01-11 16:06:49 +0800

    test on shunfa's nb and debugs

---
+++ v2.1/webmanager/src/nchc/fslab/crawlzilla/bean/crawlBean.java
@@ -0,0 +1,14 @@
+package nchc.fslab.crawlzilla.bean;
+
+public class crawlBean {
+
+	// crawl
+	// bin/nutch crawl urls /opt/crawlzilla/crawlDB/nchc-3-5 -depth 3 -topN 5
+	
+	
+	// solrindex
+	// bin/nutch solrindex http://127.0.0.1:8983/solr/collection1 /opt/crawlzilla/crawlDB/nchc-3-5/crawldb -linkdb /opt/crawlzilla/crawlDB/nchc-3-5/linkdb /opt/crawlzilla/crawlDB/nchc-3-5/segments/*
+	
+	
+	
+}
