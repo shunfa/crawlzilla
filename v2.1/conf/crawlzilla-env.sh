e947323c4e5eae3149f188be9ff70bd071d9ffa7
shunfa <shunfa@gmail.com>
2013-01-14 17:16:58 +0800

    add conf of env

---
+++ v2.1/conf/crawlzilla-env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export CRAWLZILLA_HOME="/opt/crawlzilla"
+export PACKAGES_HOME="/opt/crawlzilla/packages"
+export NUTCH_PKG="apache-nutch-1.6-bin.tar.gz"
+export SOLR_PKG="apache-solr-4.0.0.tgz"
+export CRAWLDB="/opt/crawlzilla/crawlDB"
+export NUTCH_HOME="/opt/crawlzilla/nutch"
+export TOMCAT_HOME="/opt/crawlzilla/tomcat"
+export SOLR_HOME="/opt/crawlzilla/solr"
+export URLS="/opt/crawlzilla/nutch/urls"
