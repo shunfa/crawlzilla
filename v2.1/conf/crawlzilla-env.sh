b6bfe562255630652e2ec7db2385d5b63d5cabcc
shunfa <shunfa@gmail.com>
2013-01-16 09:48:15 +0800

    edit links

--- v2.1/conf/crawlzilla-env.sh
+++ v2.1/conf/crawlzilla-env.sh
@@ -8,3 +8,9 @@ export NUTCH_HOME="/opt/crawlzilla/nutch"
 export TOMCAT_HOME="/opt/crawlzilla/tomcat"
 export SOLR_HOME="/opt/crawlzilla/solr"
 export URLS="/opt/crawlzilla/nutch/urls"
+
+## Package URLs
+export NUTCH_PKG_LINK="http://sourceforge.net/projects/crawlzilla/files/original-package/apache-nutch-1.6-bin.tar.gz/download"
+export SOLR_PKG_LINK="http://sourceforge.net/projects/crawlzilla/files/original-package/apache-solr-4.0.0.tgz/download"
+export TOMCAT_LINK=""
+export CZL_WEB_LINK=""
